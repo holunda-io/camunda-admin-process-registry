381068f4aeb30f9fcd10bb1676eaf3fb57ddabde
Jan Galinski <jan.galinski@holisticon.de>
2024-07-10 21:08:52 +0200

    update camunda-data 1.5.0
    fixes #12

--- src/main/kotlin/io/holunda/camunda/platform/adminprocess/form/LongField.kt
+++ src/main/kotlin/io/holunda/camunda/platform/adminprocess/form/LongField.kt
@@ -5,4 +5,4 @@ import io.holunda.camunda.bpm.data.CamundaBpmDataKotlin
 data class LongField(
   override val id: String,
   override val label: String
-) : FormField<Long>(id, label, null, "long", CamundaBpmDataKotlin.longVariable(id))
+) : FormField<Long>(id, label, null, "long", CamundaBpmDataKotlin.longVariable(id).nonNull)
